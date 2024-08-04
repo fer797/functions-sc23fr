2179478174f62df6b3af52b4e79a93b3fe70f804
Fernando Rodriguez <sc23fjrl@leeds.ac.uk>
2024-08-04 12:28:58 -0500

    new data fhir

--- fast/app/app.py
+++ fast/app/app.py
@@ -25,6 +25,22 @@ def background_print_task():
         time.sleep(1)
         print(f"Background task running...{i}")
 
+def background_fhir_task():
+    logging.info("Starting data ingestion and processing")
+    try:
+        medications = data_ingestion.fetch_medications()
+        logging.info(f"Fetched {len(medications)} medications")
+        transformed_data = []
+        for data in medications:
+            medication = MedicationTransformation.validate_and_create_medication(dict(data))
+            if medication:
+                medication_service.insert_medication(medication)
+                transformed_data.append(medication.dict())
+        logging.info("Data ingestion and processing completed")
+    except Exception as e:
+            logging.error(f"Exception during data ingestion and processing: {e}")
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "World yy877 feer7"}
@@ -33,6 +49,11 @@ def read_root():
 def run_background_task(background_tasks: BackgroundTasks):
     background_tasks.add_task(background_print_task)
     return {"message": "Background task started"}
+
+@app.get("/background-data")
+def run_background_task(background_tasks: BackgroundTasks):
+    background_tasks.add_task(background_fhir_task)
+    return {"message": "ACK"}
 
 @app.get('/ingest_and_process')
 def ingest_and_process_data():
